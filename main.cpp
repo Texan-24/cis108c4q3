dae4706e686f4f5f033c91b2a754360bfb5d00f0
Texan24 <84364670+Texan-24@users.noreply.github.com>
2021-06-20 16:26:26 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout; 
+
+float Days_old;
+
+int main () {
+  int my_age = 33;
+  const int NUM_DAYS_IN_YEAR = 365;
+  // my age multiplied by 365
+ 
+ Days_old = 33 * 365;
+
+  cout << "My age in days is " << Days_old ;
+}
